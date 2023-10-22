13e3ca6dfdee83c2cae73c90d5459322c126adc6
gil-son <gilszon@gmail.com>
2023-10-21 21:31:08 -0300

    Publish Message

--- user/src/main/java/com/ms/user/dtos/EmailDto.java
+++ user/src/main/java/com/ms/user/dtos/EmailDto.java
@@ -1,2 +1,52 @@
-package com.ms.user.dtos;public class EmailDto {
+package com.ms.user.dtos;
+
+import java.util.UUID;
+
+public class EmailDto {
+
+    private UUID userID;
+    private String emailTo;
+    private String subject;
+    private String text;
+
+    public EmailDto(){}
+
+    public EmailDto(UUID userID, String emailTo, String subject, String text) {
+        this.userID = userID;
+        this.emailTo = emailTo;
+        this.subject = subject;
+        this.text = text;
+    }
+
+    public UUID getUserID() {
+        return userID;
+    }
+
+    public void setUserID(UUID userID) {
+        this.userID = userID;
+    }
+
+    public String getEmailTo() {
+        return emailTo;
+    }
+
+    public void setEmailTo(String emailTo) {
+        this.emailTo = emailTo;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
--- user/src/main/java/com/ms/user/producers/UserProducer.java
+++ user/src/main/java/com/ms/user/producers/UserProducer.java
@@ -1,2 +1,33 @@
-package com.ms.user.producers;public class UserProducer {
+package com.ms.user.producers;
+
+import com.ms.user.models.UserModel;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import com.ms.user.dtos.EmailDto;
+
+@Component
+public class UserProducer {
+
+    final RabbitTemplate rabbitTemplate;
+
+    public UserProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Value("${broker.queue.email.name}")
+    private String routingKey;
+
+    public void publishMessageEmail(UserModel userModel){
+        var emailDto = new EmailDto();
+        emailDto.setUserID(userModel.getUserId());
+        emailDto.setEmailTo(userModel.getEmail());
+        emailDto.setSubject("Register was realized with success");
+        emailDto.setText(userModel.getName() + ", welcome! \n We are pleased with your registration! Enjoy it!");
+
+        rabbitTemplate.convertAndSend("", routingKey, emailDto);
+    }
+
+
+
 }
